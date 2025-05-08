96f22d263bb01d190b11694a5cb106cc261f9d32
rizkirakasiwi <rizkirakasiwi09@gmail.com>
2025-05-08 13:27:02 +0700

    remove generated file from gitignore

---
+++ config/lib/env_config.g.dart
@@ -0,0 +1,153 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env_config.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env.dev
+final class _EnvDev {
+  static const List<int> _enviedkeybaseUrl = <int>[
+    1191324349,
+    1412742108,
+    2989833657,
+    2309531663,
+    2363331824,
+    1544864198,
+    1566852866,
+    4141344970,
+    1448918208,
+    2252122088,
+    2201604730,
+    1969902007,
+    1028070056,
+    2774741705,
+    1657146641,
+    2336159993,
+    1360352735,
+    3139423363,
+    2379424909,
+    3167702100,
+    3824612100,
+    927249398,
+    103911612,
+    2883984713,
+    1601542082,
+    2136099909,
+    2114939591,
+  ];
+
+  static const List<int> _envieddatabaseUrl = <int>[
+    1191324373,
+    1412742056,
+    2989833677,
+    2309531775,
+    2363331715,
+    1544864252,
+    1566852909,
+    4141344997,
+    1448918180,
+    2252121997,
+    2201604620,
+    1969901977,
+    1028070089,
+    2774741689,
+    1657146744,
+    2336159959,
+    1360352698,
+    3139423483,
+    2379425004,
+    3167702073,
+    3824612212,
+    927249306,
+    103911641,
+    2883984743,
+    1601542049,
+    2136099882,
+    2114939562,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static final bool _enviedkeyenableLogging = false;
+
+  static final bool enableLogging = _enviedkeyenableLogging ^ true;
+}
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// generated_from: .env.prod
+final class _EnvProd {
+  static const List<int> _enviedkeybaseUrl = <int>[
+    2397122192,
+    1586253858,
+    1908552764,
+    2093384347,
+    2714061682,
+    1712219868,
+    1494489793,
+    1614671150,
+    3944727380,
+    2776415038,
+    1546606518,
+    3136048140,
+    2244783821,
+    834275195,
+    58816729,
+    2822737186,
+    739023314,
+    1528816558,
+    2883279481,
+    138430908,
+    4199781235,
+    3892003786,
+    662930679,
+  ];
+
+  static const List<int> _envieddatabaseUrl = <int>[
+    2397122296,
+    1586253910,
+    1908552776,
+    2093384427,
+    2714061569,
+    1712219878,
+    1494489838,
+    1614671105,
+    3944727349,
+    2776415054,
+    1546606559,
+    3136048162,
+    2244783784,
+    834275075,
+    58816696,
+    2822737231,
+    739023266,
+    1528816578,
+    2883279388,
+    138430866,
+    4199781136,
+    3892003749,
+    662930586,
+  ];
+
+  static final String baseUrl = String.fromCharCodes(
+    List<int>.generate(
+      _envieddatabaseUrl.length,
+      (int i) => i,
+      growable: false,
+    ).map((int i) => _envieddatabaseUrl[i] ^ _enviedkeybaseUrl[i]),
+  );
+
+  static final bool _enviedkeyenableLogging = false;
+
+  static final bool enableLogging = _enviedkeyenableLogging ^ false;
+}
